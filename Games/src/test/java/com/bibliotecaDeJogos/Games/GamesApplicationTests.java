8d4b440c1f5caca968213a6c074e05c9d43fcbd9
joseca <91603735+vjasu@users.noreply.github.com>
2025-09-23 10:12:01 -0300

    Estrutura principal do projeto
    Aqui está os arquivos principais do projeto de biblioteca de jogos com spring boot

---
+++ Games/src/test/java/com/bibliotecaDeJogos/Games/GamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bibliotecaDeJogos.Games;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
